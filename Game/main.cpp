5645e554b0093e6b6fb506c6622c1e5b66434656
Leon Lowe <leonlowe0@outlook.com>
2022-08-19 17:23:51 +0100

    Movement
    Added the following:
     - Player movement
     - Player sprinting

--- Game/main.cpp
+++ Game/main.cpp
@@ -1,4 +1,5 @@
 #include "raylib.h"
+#include "raymath.h"
 
 int main()
 {
@@ -12,7 +13,12 @@ int main()
     Texture2D mapTexture = LoadTexture("nature_tileset/OpenWorldMap.png");
     Vector2 mapPosition{0.0, 0.0};
     float mapRotation = 0.0;
-    float mapScale = 4.0;
+    float mapScale = 2.0;
+
+    const float WALK_SPEED = 4.0;
+    const float SPRINT_SPEED = 8.0;
+
+    bool sprinting = false;
 
     SetTargetFPS(60);
     bool running = true;
@@ -30,6 +36,20 @@ int main()
         BeginDrawing();
 
         ClearBackground(WHITE);
+
+        Vector2 moveDir{};
+        if(IsKeyDown(KEY_A)) {moveDir.x -= 1.0;}
+        if(IsKeyDown(KEY_D)) {moveDir.x += 1.0;}
+        if(IsKeyDown(KEY_W)) {moveDir.y -= 1.0;}
+        if(IsKeyDown(KEY_S)) {moveDir.y += 1.0;}
+
+        if(IsKeyDown(KEY_LEFT_SHIFT)) {sprinting = true;}
+        if(!IsKeyDown(KEY_LEFT_SHIFT)) {sprinting = false;}
+        
+        if(Vector2Length(moveDir) != 0.0)
+        {
+            mapPosition = Vector2Subtract(mapPosition, Vector2Scale(Vector2Normalize(moveDir), sprinting ? SPRINT_SPEED : WALK_SPEED));
+        }
 
         DrawTextureEx(mapTexture, mapPosition, mapRotation, mapScale, WHITE);
 
